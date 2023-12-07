c48233e21c679810b7cbaea925f24166cac1ca59
Demidb <LLiker1337@gmail.com>
2023-12-07 20:38:10 +0300

    2 brain_games/scripts/brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,16 @@
+import prompt
+import random
 def even():
-    for i in range(3):
+    print(f'Answer "yes" if the number is even, otherwise answer "no"')
+    count = 0
+    for n in range(3):
         number = random.randint(1, 100)
-            if (number % 2 == 0 and answer == 'yes') or (number % 2 != 0 and answer == 'no'):
+        print(f'Question: {number}')
+        answer = input()
+        print(f'Your answer: {answer}')
+        if number % 2 == 0 :
                 print('Correct!')
-                pass
-        
+                count +=1
+    if count == 3:
                     print(f'Congratulations, {name}!')
-    print (f"'yes' is wrong answer ;(. Correct answer was 'no'./nLet's try again, Bill!")
+    print (f"'yes' is wrong answer ;(. Correct answer was 'no'./nLet's try again, {name}")
