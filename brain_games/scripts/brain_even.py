a40c61f0c81e5f9c1619053106dd2cffd8a0b158
Demidb <LLiker1337@gmail.com>
2023-12-07 19:09:41 +0300

    2 brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,5 +3,7 @@ def even():
         number = random.randint(1, 100)
             if (number % 2 == 0 and answer == 'yes') or (number % 2 != 0 and answer == 'no'):
                 print('Correct!')
+                pass
+        
                     print(f'Congratulations, {name}!')
     print (f"'yes' is wrong answer ;(. Correct answer was 'no'./nLet's try again, Bill!")
