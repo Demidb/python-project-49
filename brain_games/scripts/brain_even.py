83cbe4a551f1a7cc58d20df96365d16a3187c34c
Demidb <LLiker1337@gmail.com>
2023-12-07 21:10:58 +0300

    2 brain_games/scripts/brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,16 +1,21 @@
 import prompt
 import random
 def even():
-    print(f'Answer "yes" if the number is even, otherwise answer "no"')
+    name = prompt.string('Welcome to the Brain Games!\n'
+                         'May I have your name? ')
+    print(f"Hello, {name}!\n"
+          f'Answer "yes" if the number is even, otherwise answer "no"')
     count = 0
     for n in range(3):
         number = random.randint(1, 100)
         print(f'Question: {number}')
         answer = input()
         print(f'Your answer: {answer}')
-        if number % 2 == 0 :
+        if (number % 2 == 0 and answer == 'yes') or (number % 2 != 0 and answer == 'no'):
                 print('Correct!')
                 count +=1
     if count == 3:
                     print(f'Congratulations, {name}!')
-    print (f"'yes' is wrong answer ;(. Correct answer was 'no'./nLet's try again, {name}")
+    else:
+           print (f"'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {name}")
+even()
