c7c9ec472cedba0ce876fd41ca7722688f0df105
Demidb <LLiker1337@gmail.com>
2023-12-08 13:22:57 +0300

    4 pyproject.toml

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,12 @@
 from brain_games.cli import welcome_user
 from brain_games.calc import calc
 from brain_games.even import even
-
+from brain_games.gcd import gc1
 def main():
     welcome_user()  # наша функция
+    calc()
+    even()
+    gcd1()
 
 if __name__ == '__main__':
 
