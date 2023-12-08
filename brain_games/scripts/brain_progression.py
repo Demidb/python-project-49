cf1a39b6a9cbf98739dca9da23e5cfad49db4cbe
Demidb <LLiker1337@gmail.com>
2023-12-08 19:51:11 +0300

    brain_gcd.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -29,7 +29,9 @@ def main():
             print('Correct!')
             count += 1
         else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{true_answer}'.\nLet's try again, {name}")
+                        print (f" {answer} is wrong answer ;(. Correct answer was {true_answer}.\nLet's try again, {name}!")
+                        break
 
     if count == 3:
         print(f'Congratulations, {name}!')
+main()
