2043785c805e01d3c5de3099819941d3f13a73ef
Demidb <LLiker1337@gmail.com>
2023-12-09 12:18:09 +0300

    5 autotest

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,32 +1,38 @@
 import random
 
 
-def main():
-    def is_prime(number):
-        if number < 2:
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
             return False
-        for i in range(2, int(number**0.5) + 1):
-            if number % i == 0:
-                return False
-        return True
+    return True
+
+
+def main():
     count = 0
     name = input('Welcome to the Brain Games!\n'
                  'May I have your name? ')
     print(f"Hello, {name}!\n"
           f'Answer "yes" if given number is prime. Otherwise answer "no".')
-    for n in range(3):
+    for _ in range(3):
         number = random.randint(2, 50)
         print(f'Question: {number}')
         user_answer = input('Your answer: ').lower()
         if user_answer in ('yes', 'no'):
             correct_answer = 'yes' if is_prime(number) else 'no'
-
             if user_answer == correct_answer:
                 print("Correct!")
                 count += 1
             else:
-                print(f" {user_answer} is wrong answer ;(. Correct answer was {correct_answer}."
+                print(f"{user_answer} is wrong answer ;(. "
+                      f"Correct answer was {correct_answer}."
                       f"\nLet's try again, {name}!")
                 break
     if count == 3:
         print(f'Congratulations, {name}!')
+
+
+if __name__ == "__main__":
+    main()
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,6 +1,7 @@
 import prompt
 import random
 
+
 def main():
     def hidden_number(progression):
         hidden_index = random.randint(0, len(progression) - 1)
@@ -18,10 +19,8 @@ def main():
         step = random.randint(1, 10)
         first_number = random.randint(0, 100)
         progression_list = [first_number + i * step for i in range(length)]
-
         hidden_prog, hidden_number_value = hidden_number(progression_list)
-
-        print(f'Question:', ' '.join(map(str, hidden_prog)))
+        print("Question:", " ".join(map(str, hidden_prog)))
         answer = prompt.string('Your answer: ')
         true_answer = str(hidden_number_value)
 
@@ -29,7 +28,8 @@ def main():
             print('Correct!')
             count += 1
         else:
-            print(f" {answer} is wrong answer ;(. Correct answer was {true_answer}."
+            print(f" {answer} is wrong answer ;(. "
+                  f"Correct answer was {true_answer}."
                   f"\nLet's try again, {name}!")
             break
 
