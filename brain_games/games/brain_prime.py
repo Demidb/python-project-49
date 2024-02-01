e90bd1fe08cc80c0369d6afc7f4ae2f855baebcc
Demidb <LLiker1337@gmail.com>
2024-02-01 15:38:31 +0300

    flake2

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -7,11 +7,10 @@ from brain_games.consts import PRIME_INSTUCTION
 def is_prime(num):
     if num < 2:
         return False
-
     for i in range(2, int(num**0.5) + 1):
         if num % i == 0:
             return False
-        return True
+    return True
 
 
 def get_num_and_prime_ans():
